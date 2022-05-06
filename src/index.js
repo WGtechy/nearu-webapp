834e81a91de561f35983ab41b02df7f685db8caf
Williams Godwin <goddy006@yahoo.com>
2022-05-06 20:33:38 +0100

    New branch

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}> <App /> </Provider>
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,9 +9,8 @@ import rootReducer from "./reducers/rootReducer"
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Middleware for initial signin
-const userInfoStorage = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null
 //the create store takes 2 arguments first is the reducer and the second is the middleware. 9n this case im using thunk as the middleware
- const store = createStore(rootReducer, userInfoStorage, composeEnhancer(applyMiddleware(thunk)));
+ const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 
 // viewing the store in the browser for development
